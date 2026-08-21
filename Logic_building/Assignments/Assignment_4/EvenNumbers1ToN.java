9ec1b1282eea07e7bd92323fa1a791529b4fe15c
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2026-08-21 17:29:20 +0530

    Assignment 4: Solve Question 4 - Print even numbers from 1 to N

---
+++ Logic_building/Assignments/Assignment_4/EvenNumbers1ToN.java
@@ -0,0 +1,17 @@
+package Logic_building.Assignments.Assignment_4;
+import java.util.Scanner;
+
+public class EvenNumbers1ToN {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        for(int i=1 ; i<=num ; i++){
+            if(i%2==0){
+                System.out.print(i+" ");
+            }
+        }
+        sc.close();
+    }
+}
