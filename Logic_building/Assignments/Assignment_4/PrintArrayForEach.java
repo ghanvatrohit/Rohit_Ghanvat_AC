d38c7acaa4bd57fc79dc24692a5fb57dedda7fe8
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2026-08-21 20:24:28 +0530

    Assignment 4: Solve Question 6 - Print array elements using for-each loop

---
+++ Logic_building/Assignments/Assignment_4/PrintArrayForEach.java
@@ -0,0 +1,23 @@
+package Logic_building.Assignments.Assignment_4;
+import java.util.Scanner;
+
+public class PrintArrayForEach {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = 5;
+
+        int[] arr = new int[n];
+        System.out.print("Enter "+n+" integers: ");
+        for(int i=0 ; i<n ;i++){
+            arr[i] = sc.nextInt();
+        }
+        
+        // for(int i=0 ; i<n ;i++){
+            for(int num : arr){
+            System.out.print(num +" ");
+        }
+        sc.close();
+
+    }
+}
