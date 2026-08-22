05fd01bc272b2e206f63ba2219eb025267e5f5de
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2026-08-22 19:39:30 +0530

    Assignment 4: Solve Question 16 - Print the given number pattern

---
+++ Logic_building/Assignments/Assignment_4/NumberStarPattern.java
@@ -0,0 +1,20 @@
+package Logic_building.Assignments.Assignment_4;
+import java.util.Scanner;
+
+public class NumberStarPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Value: ");
+        int n = sc.nextInt();
+
+        for(int i=1 ; i<=n ; i++){
+            for(int j=1 ; j<=i ; j++){
+                System.out.print(j);
+                if(j<i){
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }sc.close();
+    }
+}
