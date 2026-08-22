87d323030f0b75ba60cad34b29d0601ad05b49f0
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2026-08-22 22:18:03 +0530

    Assignment 4: Solve Question 23 - Print the given diamond star pattern

---
+++ Logic_building/Assignments/Assignment_4/DiamondStarPattern.java
@@ -0,0 +1,31 @@
+package Logic_building.Assignments.Assignment_4;
+
+import java.util.Scanner;
+
+public class DiamondStarPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Value : ");
+        int n = sc.nextInt();
+
+        for(int i=1 ; i<=n ; i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int k=1; k<=2*i-1; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = n-1 ; i >= 1; i--) {
+            for (int j = 1; j <= n-i; j++) {
+                System.out.print(" ");
+            }
+            for(int k=1 ; k<=2*i-1 ; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
