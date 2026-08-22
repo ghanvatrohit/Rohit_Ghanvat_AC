79d1b6758e5f41dd72cfe34e1a1e4d0742eec125
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2026-08-22 20:02:04 +0530

    Assignment 4: Solve Question 18 - Print the given same number pattern

---
+++ Logic_building/Assignments/Assignment_4/SameNumberPattern.java
@@ -0,0 +1,18 @@
+package Logic_building.Assignments.Assignment_4;
+import java.util.Scanner;
+
+public class SameNumberPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Value : ");
+        int n = sc.nextInt();
+
+        for(int i=1 ; i<=n ; i++){
+            for(int j=1 ; j<=n ; j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
