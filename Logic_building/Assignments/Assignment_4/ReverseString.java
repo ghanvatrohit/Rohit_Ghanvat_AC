46b3b1ca4f89dbbabfb153763bfaeba659944698
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2026-08-22 22:26:08 +0530

    Assignment 4: Solve Question 24 - Reverse a string

---
+++ Logic_building/Assignments/Assignment_4/ReverseString.java
@@ -0,0 +1,15 @@
+package Logic_building.Assignments.Assignment_4;
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string : ");
+        String str = sc.nextLine();
+
+        for(int i=str.length()-1 ; i>=0 ; i--){
+            System.out.print(str.charAt(i));
+        }
+        sc.close();
+    }
+}
