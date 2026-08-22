199ef10c395b272577c904f50265996da3eae0f5
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2026-08-22 14:11:52 +0530

    Assignment 4: Update Question 10 - Find the average of elements in an array

--- Logic_building/Assignments/Assignment_4/AverageOfArray.java
+++ Logic_building/Assignments/Assignment_4/AverageOfArray.java
@@ -6,7 +6,7 @@ public class AverageOfArray {
         Scanner sc = new Scanner(System.in);
         int n=5;
         int arr[] = new int[n];
-        System.out.print("Enter "+5+" itegers: ");
+        System.out.print("Enter "+5+" integers: ");
         for(int i=0 ; i<n ; i++){
             arr[i] = sc.nextInt();
         }
