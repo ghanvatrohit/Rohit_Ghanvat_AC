a2fa49e2a2be4b9bfa5c56aa262daf3ead1b719f
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2026-08-24 18:20:41 +0530

    Add program to calculate sum of two numbers

---
+++ C++_Assinment/Day_1/add_numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+
+    int num1 , num2;
+
+    std::cout << "Enter two numbers separated by space: ";
+    std::cin >> num1 >> num2;
+
+    std::cout << "Addition of two numbers: "<< (num1+num2) <<std::endl;
+}
