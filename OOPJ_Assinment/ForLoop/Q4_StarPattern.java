56341ba562515832c4e11f1381ef05750b2bfb97
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2026-09-14 19:49:28 +0530

    Add star pattern program

---
+++ OOPJ_Assinment/ForLoop/Q4_StarPattern.java
@@ -0,0 +1,16 @@
+package OOPJ_Assinment.ForLoop;
+
+public class Q4_StarPattern {
+    public static void main(String[] args) {
+
+        for (int iTemp = 1; iTemp <= 5; iTemp++) {
+
+            for (int jTemp = 1; jTemp <= iTemp; jTemp++) {
+
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
