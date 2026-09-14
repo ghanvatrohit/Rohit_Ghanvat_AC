b5fae65e6ae1d5dd88016f8a050b5a5393e564e5
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2026-09-14 19:39:05 +0530

    Add array average program

---
+++ OOPJ_Assinment/Array/Q4_ArrayAverage.java
@@ -0,0 +1,21 @@
+public class Q4_ArrayAverage {
+	public static void main(String[] args) {
+		System.out.println("Enter size of array: ");
+		int size = ConsoleInput.getInt();
+		
+		Q4_ArrayAverageLogic obj = new Q4_ArrayAverageLogic(size);
+		
+		System.out.println("Enter "+size+" numbers:");
+		
+		for(int iTemp=0 ; iTemp<size ; iTemp++) {
+			obj.setNumber(iTemp,ConsoleInput.getInt());
+		}
+		
+		System.out.println("\n--- Array ----");
+		obj.displayArray();
+		
+		double result = obj.calculateAverage();
+		
+		System.out.println("\nAverage of array = "+ result);
+	}
+}
---
+++ OOPJ_Assinment/Array/Q4_ArrayAverageLogic.java
@@ -0,0 +1,32 @@
+public class Q4_ArrayAverageLogic {
+	private int[] numbers;
+	
+	//constructor
+	public Q4_ArrayAverageLogic(int size) {
+		numbers = new int[size];
+		
+	}
+	
+	//setter
+	public void setNumber(int index , int value) {
+		numbers[index] = value;
+	}
+	
+	//calculate averag
+	public double calculateAverage() {
+		int total = 0 ;
+		
+		for(int iTemp = 0 ; iTemp < numbers.length; iTemp++) {
+			total = total + numbers[iTemp];
+		}
+		return (double)total/numbers.length;
+	}
+	
+	//Display Array
+	public void displayArray() {
+		for(int iTemp=0 ; iTemp<numbers.length;iTemp++) {
+			System.out.print(numbers[iTemp]+" ");
+		}
+		System.out.println();
+	}
+}
