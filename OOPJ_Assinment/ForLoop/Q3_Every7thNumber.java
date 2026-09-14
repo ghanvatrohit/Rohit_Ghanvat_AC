1d47da83eca91d83fd75b5d4c9f23898f4548716
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2026-09-14 19:48:33 +0530

    Add program to display every 7th number

---
+++ OOPJ_Assinment/ForLoop/Q3_Every7thNumber.java
@@ -0,0 +1,11 @@
+package OOPJ_Assinment.ForLoop;
+
+public class Q3_Every7thNumber {
+    public static void main(String[] args) {
+
+        for (int iTemp = 7; iTemp <= 200; iTemp = iTemp + 7) {
+
+            System.out.println(iTemp);
+        }
+    }
+}
