34328e0a759ed8ce9c496fc54df526b1087c61e8
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2026-09-14 19:47:50 +0530

    Add program to display even numbers

---
+++ OOPJ_Assinment/ForLoop/Q2_EvenNumbers.java
@@ -0,0 +1,13 @@
+package OOPJ_Assinment.ForLoop;
+
+public class Q2_EvenNumbers {
+    public static void main(String[] args) {
+
+        for (int iTemp = 1; iTemp <= 500; iTemp++) {
+
+            if (iTemp % 2 == 0) {
+                System.out.println(iTemp);
+            }
+        }
+    }
+}
