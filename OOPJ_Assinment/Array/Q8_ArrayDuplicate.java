d5a4b7269ca855939cf97cef4ffe09129e8c2dc6
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2026-09-14 19:39:24 +0530

    Add duplicate values program

---
+++ OOPJ_Assinment/Array/Q8_ArrayDuplicate.java
@@ -0,0 +1,20 @@
+public class Q8_ArrayDuplicate {
+	public static void main(String[] args) {
+		System.out.println("Enter size of array: ");
+		int size = ConsoleInput.getInt();
+		
+		Q8_ArrayDuplicateLogic obj = new Q8_ArrayDuplicateLogic(size);
+		
+		System.out.println("\nEnter "+size+" numbers:");
+		
+		for(int iTemp = 0 ; iTemp<size; iTemp++) {
+			obj.setNumber(iTemp,ConsoleInput.getInt());
+		}
+		
+		System.out.println("\n--- original Array ---");
+		obj.displayArray();
+		
+		System.out.println("\n--- Duplicate Values ---");
+		obj.findDuplicates();
+	}
+}
---
+++ OOPJ_Assinment/Array/Q8_ArrayDuplicateLogic.java
@@ -0,0 +1,29 @@
+public class Q8_ArrayDuplicateLogic {
+	private int[] numbers;
+	
+	public Q8_ArrayDuplicateLogic(int size) {
+		 numbers = new int[size];
+	}
+	
+	public void setNumber(int index, int value) {
+		numbers[index]= value;
+	}
+	
+	public void displayArray() {
+		for(int iTemp=0 ; iTemp<numbers.length; iTemp++) {
+			System.out.print(numbers[iTemp]+" ");
+		}
+		System.out.println();
+	}
+	
+	public void findDuplicates() {
+		for(int iTemp=0 ; iTemp<numbers.length-1;iTemp++) {
+			for(int jTemp=iTemp+1; jTemp<numbers.length;jTemp++) {
+				if(numbers[iTemp]==numbers[jTemp]) {
+					System.out.println("Duplicate value = "+numbers[iTemp]);
+					break;
+				}
+			}
+		}
+	}
+}
