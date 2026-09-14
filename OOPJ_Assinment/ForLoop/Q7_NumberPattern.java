37216a1053bebc8bef7419efe086aad892bec42c
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2026-09-14 19:51:18 +0530

    Add number pattern program

---
+++ OOPJ_Assinment/ForLoop/Q7_NumberPattern.java
@@ -0,0 +1,16 @@
+package OOPJ_Assinment.ForLoop;
+
+public class Q7_NumberPattern {
+    public static void main(String[] args) {
+
+        for (int iTemp = 1; iTemp <= 6; iTemp++) {
+
+            for (int jTemp = 1; jTemp <= iTemp; jTemp++) {
+
+                System.out.print(jTemp + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
