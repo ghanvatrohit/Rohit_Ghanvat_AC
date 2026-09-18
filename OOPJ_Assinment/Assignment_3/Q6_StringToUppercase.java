ea1f3af6a80669b9d2313701606048cf64f20155
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2026-09-18 22:30:16 +0530

    Add program to convert string to uppercase

---
+++ OOPJ_Assinment/Assignment_3/Q6_StringToUppercase.java
@@ -0,0 +1,15 @@
+package Assignment_3;
+
+public class Q6_StringToUppercase {
+
+    public static void main(String[] args) {
+
+        String str = "The Quick BroWn FoX!";
+
+        System.out.println("Original String: " + str);
+
+        String upperCaseString = str.toUpperCase();
+
+        System.out.println("String in uppercase: " + upperCaseString);
+    }
+}
