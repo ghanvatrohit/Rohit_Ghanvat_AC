4b5446d2df94c3dbed4b6d42871fb7042297869c
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2026-09-21 16:48:41 +0530

    Add ArrayList color collection program

---
+++ OOPJ_Assinment/ArrayList/Q1_CreateArrayList.java
@@ -0,0 +1,23 @@
+package ArrayList;
+
+import java.util.ArrayList;
+
+public class Q1_CreateArrayList {
+
+    public static void main(String[] args) {
+
+        // Create an ArrayList of String
+        ArrayList<String> colors = new ArrayList<>();
+
+        // Add colors to the ArrayList
+        colors.add("Red");
+        colors.add("Blue");
+        colors.add("Green");
+        colors.add("Yellow");
+        colors.add("Orange");
+
+        // Print the collection
+        System.out.println("Colors: " + colors);
+    }
+}
+
